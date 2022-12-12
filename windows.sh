fadb16dd7ed1899cd523ab1861b1075b52ae3047
X - MrG3P5 <57594747+MrG3P5@users.noreply.github.com>
2022-12-12 15:38:51 +0700

    update windows.sh

--- windows.sh
+++ windows.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 echo "SCRIPT AUTO INSTALL WINDOWS"
 echo
-echo "Pilih OS yang ingin anda install"
-echo "[1] Windows 2019(Default)"
-echo "[2] Windows 2016"
-echo "[3] Windows 2012"
-echo "[4) Windows 10"
-echo "[5] Pakai link gz mu sendiri"
-
-read -p "Pilih [1]: " PILIHOS
-
-case "$PILIHOS" in
-	1|"") PILIHOS="https://file.nixpoin.com/windows2019DO.gz";;
-	2) PILIHOS="https://file.nixpoin.com/windows2016.gz";;
-	3) PILIHOS="https://file.nixpoin.com/windows2012v2.gz";;
-	4) PILIHOS="https://file.nixpoin.com/win10.gz";;
-	5) read -p "[?] Masukkan Link GZ mu : " PILIHOS;;
-	*) echo "[!] Pilihan salah"; exit;;
-esac
-
-echo "[*] Password yang saya buat sudah masuk wordlist bruteforce, silahkan masukkan password yang lebih aman!"
-read -p "[?] Masukkan password untuk akun Administrator (minimal 12 karakter) : " PASSADMIN
-
+PILIHOS="https://file.nixpoin.com/windows2019DO.gz"
+PASSADMIN="Kijang123456"
 IP4=$(curl -4 -s icanhazip.com)
 GW=$(ip route | awk '/default/ { print $3 }')
 
